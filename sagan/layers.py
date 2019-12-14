a3c211187e59806d75ee79de3613da86beeea2d2
JimmYA-1995 <yct1995.cai08g@nctu.edu.tw>
2019-12-14 15:09:53 +0000

    fix init_scope error

--- sagan/layers.py
+++ sagan/layers.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers
-        
+
 def l2normalize(v, eps=1e-12):
     return tf.math.divide(v,(tf.norm(v) + eps))
 
@@ -28,9 +28,9 @@ class SpectralNormalization(layers.Layer):
             return False
 
     def _make_param(self):
-        W = getattr(self.module, self.weight_name)[0]
-        height = W.shape[-1]
-        width = tf.reshape(W, shape=(height, -1)).shape[1]
+        w = getattr(self.module, self.weight_name)[0]
+        height = w.shape[-1]
+        width = tf.reshape(w, shape=(height, -1)).shape[1]
 
         u = tf.random.normal(shape=[1, height])
         v = tf.random.normal(shape=[1, width])
@@ -47,20 +47,19 @@ class SpectralNormalization(layers.Layer):
             self.update_uv()
         return self.module.call(x)
 
-    # @tf.function
+    @tf.function
     def update_uv(self):
         """ Spectrally Normalized Weight
         """
         W = getattr(self.module, self.weight_name)[0]
-        
-        with tf.init_scope():
-            W_mat = tf.reshape(W, [W.shape[-1], -1])
-        
-            for _ in range(self.Ip):
-                self.v = l2normalize(tf.matmul(self.u, W_mat))
-                self.u = l2normalize(tf.matmul(self.v, tf.transpose(W_mat)))
+        W_mat = tf.reshape(W, [W.shape[-1], -1])
+
+        for _ in range(self.Ip):
+            self.v = l2normalize(tf.matmul(self.u, W_mat))
+            self.u = l2normalize(tf.matmul(self.v, tf.transpose(W_mat)))
             
-            sigma = tf.reduce_sum(tf.matmul(self.u, W_mat) * self.v)
+        sigma = tf.reduce_sum(tf.matmul(self.u, W_mat) * self.v)
+
 
         if self.factor:
             sigma = sigma / self.factor
